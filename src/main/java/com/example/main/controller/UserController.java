2106f8574639057b841bd87fe995af8340242fa0
tbin <TBinTB0003@gmail.com>
2025-05-17 01:29:31 +0700

    Update the validate request body

--- src/main/java/com/example/main/controller/UserController.java
+++ src/main/java/com/example/main/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.main.controller;
 
 import com.example.main.dto.request.UserRequestDTO;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,35 +10,32 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
     @PostMapping()
-    public String addUser(@RequestBody UserRequestDTO userDTO){
+    public String addUser(@Valid @RequestBody UserRequestDTO userDTO) {
         return "User added";
     }
 
     @PutMapping("/{userID}")
-    public String updateUser(@PathVariable int userID, @RequestBody UserRequestDTO userDTO){
+    public String updateUser(@PathVariable int userID, @RequestBody UserRequestDTO userDTO) {
         return "User updated";
     }
 
     @PatchMapping("/{userID}")
-    public String changeStatus(@PathVariable int userID, @RequestParam(required = false) boolean status){
+    public String changeStatus(@PathVariable int userID, @RequestParam(required = false) boolean status) {
         return "User status changed";
     }
 
     @DeleteMapping("/{userID}")
-    public String deleteUser(@PathVariable int userID){
+    public String deleteUser(@PathVariable int userID) {
         return "User deleted";
     }
 
     @GetMapping("/{userID}")
-    public UserRequestDTO getUser(@PathVariable int userID){
-        return new UserRequestDTO("","","","");
+    public UserRequestDTO getUser(@PathVariable int userID) {
+        return new UserRequestDTO("", "", "", "", null, null);
     }
 
     @GetMapping()
-    public List<UserRequestDTO> getAllUsers(
-            @RequestParam(required = false) String email,
-            @RequestParam(defaultValue = "0") int pageNo,
-            @RequestParam(defaultValue = "10") int pageSize){
+    public List<UserRequestDTO> getAllUsers(@RequestParam(required = false) String email, @RequestParam(defaultValue = "0") int pageNo, @RequestParam(defaultValue = "10") int pageSize) {
         return new ArrayList<UserRequestDTO>();
     }
 }
--- src/main/java/com/example/main/dto/request/UserRequestDTO.java
+++ src/main/java/com/example/main/dto/request/UserRequestDTO.java
@@ -1,19 +1,36 @@
 package com.example.main.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequestDTO implements Serializable {
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Pattern(regexp = "\\d{10}$", message = "Invalid phone format")
     private String phone;
 
-    @NotNull
+    @NotBlank(message = "First name must not be blank")
     private String firstName;
+
+    @NotBlank(message = "Last name must not be blank")
     private String lastName;
+
+    @NotNull(message = "Date of birth mus not be null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private Date dob;
+
+    @NotEmpty
+    private List<String> permissions;
 }
