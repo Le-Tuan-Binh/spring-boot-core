a78add8b150723a2ed11a05399ad13bc0fb9d3a0
tbin <TBinTB0003@gmail.com>
2025-05-18 03:36:08 +0700

    Update the description for swagger ui using generic type

--- src/main/java/com/example/main/controller/UserController.java
+++ src/main/java/com/example/main/controller/UserController.java
@@ -1,15 +1,11 @@
 package com.example.main.controller;
 
 import com.example.main.dto.request.UserRequestDTO;
+import com.example.main.dto.response.ResponseData;
 import com.example.main.dto.response.ResponseSuccess;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +14,9 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class UserController {
 
-    @Operation(summary = "", description = "", responses = {
-            @ApiResponse(responseCode = "201",
-                         description = "User added successfully",
-                         content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                            examples = @ExampleObject(name = "", summary = "", value = """
-                                                    {
-                                                        "status": 201,
-                                                        "message": "User added successfully",
-                                                        "data": 1
-                                                    }
-                                                    """)))
-    })
     @PostMapping()
-    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO userDTO) {
-        return new ResponseSuccess(HttpStatus.CREATED, "User added successfully", 1);
+    public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO userDTO) {
+        return new ResponseData<>(HttpStatus.CREATED.value(), "User added successfully", 1);
     }
 
     @PutMapping("/{userID}")
